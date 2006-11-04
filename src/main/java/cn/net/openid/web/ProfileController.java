51faf7be18470078437aff68ffd945fd300ba905
zhoushuqun <zhoushuqun>
2006-11-04 18:27:02 +0000

    Added profile editing function.

--- src/main/java/cn/net/openid/web/ProfileController.java
+++ src/main/java/cn/net/openid/web/ProfileController.java
@@ -3,10 +3,20 @@
  */
 package cn.net.openid.web;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
@@ -18,6 +28,7 @@ import cn.net.openid.dao.DaoFacade;
  * 
  */
 public class ProfileController extends SimpleFormController {
+
 	private DaoFacade daoFacade;
 
 	/*
@@ -34,6 +45,20 @@ public class ProfileController extends SimpleFormController {
 		String userId = userSession.getUserId();
 		User user = this.daoFacade.getUser(userId);
 		return user;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.web.servlet.mvc.BaseCommandController#initBinder(javax.servlet.http.HttpServletRequest,
+	 *      org.springframework.web.bind.ServletRequestDataBinder)
+	 */
+	@Override
+	protected void initBinder(HttpServletRequest request,
+			ServletRequestDataBinder binder) throws Exception {
+		super.initBinder(request, binder);
+		binder.registerCustomEditor(Date.class, "dob", new CustomDateEditor(
+				new SimpleDateFormat("yyyy-MM-dd"), true));
 	}
 
 	/*
@@ -58,6 +83,74 @@ public class ProfileController extends SimpleFormController {
 		User user = (User) command;
 		this.daoFacade.updateUser(user);
 		return super.onSubmit(command);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.web.servlet.mvc.SimpleFormController#referenceData(javax.servlet.http.HttpServletRequest)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	protected Map referenceData(HttpServletRequest request) throws Exception {
+		Map map = super.referenceData(request);
+		if (map == null) {
+			map = new HashMap<Object, Object>();
+		}
+
+		final Map<String, String> genders = new LinkedHashMap<String, String>();
+		final Map<String, String> timezones = new LinkedHashMap<String, String>();
+		final Map<String, String> countries = new LinkedHashMap<String, String>();
+		final Map<String, String> languages = new LinkedHashMap<String, String>();
+
+		genders.put("U", "Unknown");
+		genders.put("M", "Male");
+		genders.put("F", "Female");
+		map.put("genders", genders);
+
+		Locale[] locales = Locale.getAvailableLocales();
+		for (Locale locale : locales) {
+			countries.put(locale.getISO3Country(), locale
+					.getDisplayName(request.getLocale()));
+			languages.put(locale.getISO3Language(), locale
+					.getDisplayLanguage(request.getLocale()));
+		}
+
+		map.put("countries", countries);
+		map.put("languages", languages);
+
+		String[] timezoneIds = TimeZone.getAvailableIDs();
+		for (String timeZoneId : timezoneIds) {
+			TimeZone timeZone = TimeZone.getTimeZone(timeZoneId);
+			String s = timeZone.getDisplayName(request.getLocale());
+			int offset = timeZone.getRawOffset();
+			timezones.put(timeZoneId, String.format("(GMT%1$s) %2$s",
+					formatOffset(offset), s));
+		}
+
+		map.put("timezones", timezones);
+		return map;
+	}
+
+	private String formatOffset(int offset) {
+		int m = offset / (1000 * 60);
+		int h = m / 60;
+		StringBuffer sb = new StringBuffer();
+		if (offset > 0) {
+			sb.append('+');
+		} else {
+			sb.append('-');
+		}
+		if (Math.abs(h) < 10) {
+			sb.append('0');
+		}
+		sb.append(Math.abs(h)).append(":");
+		m = m - h * 60;
+		if (m < 10) {
+			sb.append('0');
+		}
+		sb.append(m);
+		return sb.toString();
 	}
 
 	/**
