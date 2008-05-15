271d414551cc2aadbc7dce7ba46c46fa8d577e02
zhoushuqun <zhoushuqun>
2008-05-15 17:01:22 +0000

    Fix signature while extensions added.

--- src/main/java/cn/net/openid/web/AbstractSimpleFormController.java
+++ src/main/java/cn/net/openid/web/AbstractSimpleFormController.java
@@ -3,6 +3,8 @@
  */
 package cn.net.openid.web;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 
 import cn.net.openid.dao.DaoFacade;
@@ -12,6 +14,8 @@ import cn.net.openid.dao.DaoFacade;
  * 
  */
 public abstract class AbstractSimpleFormController extends SimpleFormController {
+	protected final Log log = LogFactory.getLog(getClass());
+
 	protected DaoFacade daoFacade;
 
 	/**
--- src/main/java/cn/net/openid/web/ApprovingController.java
+++ src/main/java/cn/net/openid/web/ApprovingController.java
@@ -3,9 +3,30 @@
  */
 package cn.net.openid.web;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.openid4java.association.AssociationException;
+import org.openid4java.message.AuthRequest;
+import org.openid4java.message.AuthSuccess;
+import org.openid4java.message.DirectError;
+import org.openid4java.message.Message;
+import org.openid4java.message.MessageException;
+import org.openid4java.message.MessageExtension;
+import org.openid4java.message.ParameterList;
+import org.openid4java.message.ax.AxMessage;
+import org.openid4java.message.ax.FetchRequest;
+import org.openid4java.message.ax.FetchResponse;
+import org.openid4java.message.sreg.SRegMessage;
+import org.openid4java.message.sreg.SRegRequest;
+import org.openid4java.message.sreg.SRegResponse;
+import org.openid4java.server.ServerException;
+import org.openid4java.server.ServerManager;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,6 +39,18 @@ public class ApprovingController extends AbstractSimpleFormController {
 	/*
 	 * (non-Javadoc)
 	 * 
+	 * @see org.springframework.web.servlet.mvc.AbstractFormController#isFormSubmission(javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+	protected boolean isFormSubmission(HttpServletRequest request) {
+		return request.getParameter("allow_once") != null
+				|| request.getParameter("allow_forever") != null
+				|| request.getParameter("deny") != null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.springframework.web.servlet.mvc.SimpleFormController#onSubmit(javax.servlet.http.HttpServletRequest,
 	 *      javax.servlet.http.HttpServletResponse, java.lang.Object,
 	 *      org.springframework.validation.BindException)
@@ -26,17 +59,134 @@ public class ApprovingController extends AbstractSimpleFormController {
 	protected ModelAndView onSubmit(HttpServletRequest request,
 			HttpServletResponse response, Object command, BindException errors)
 			throws Exception {
+		Boolean approved;
 		if (request.getParameter("allow_once") != null) {
-			request.getSession().setAttribute("approved", true);
-			response.sendRedirect("server?_action=complete");
+			approved = Boolean.TRUE;
 		} else if (request.getParameter("allow_forever") != null) {
-			request.getSession().setAttribute("approved", true);
-			response.sendRedirect("server?_action=complete");
+			approved = Boolean.TRUE;
+			// TODO
+		} else if (request.getParameter("deny") != null) {
+			approved = Boolean.FALSE;
 		} else {
-			request.getSession().setAttribute("approved", false);
-			response.sendRedirect("server?_action=complete");
+			approved = Boolean.FALSE;
 		}
+
+		response(ServerServlet.getManager(this.getServletContext()), request,
+				response, (ParameterList) request.getSession().getAttribute(
+						"request"), approved);
 		return null;
 	}
 
+	private void response(ServerManager manager, HttpServletRequest httpReq,
+			HttpServletResponse httpResp, ParameterList request,
+			Boolean approved) throws MessageException, IOException {
+		Message response;
+		// interact with the user and obtain data needed to continue
+
+		String userSelectedClaimedId = null;
+		Boolean authenticatedAndApproved = approved;
+		String email = "user@example.org";
+
+		// --- process an authentication request ---
+		AuthRequest authReq = AuthRequest.createAuthRequest(request, manager
+				.getRealmVerifier());
+
+		String opLocalId = null;
+		// if the user chose a different claimed_id than the one in request
+		if (userSelectedClaimedId != null
+				&& userSelectedClaimedId.equals(authReq.getClaimed())) {
+			// opLocalId = lookupLocalId(userSelectedClaimedId);
+		}
+
+		log.debug("Start of authResponse");
+		boolean signNow = false;
+		response = manager.authResponse(request, opLocalId,
+				userSelectedClaimedId, authenticatedAndApproved.booleanValue(),
+				signNow);
+		log.debug("End of authResponse");
+
+		if (response instanceof DirectError) {
+			directResponse(httpResp, response.keyValueFormEncoding());
+		} else {
+			if (authReq.hasExtension(AxMessage.OPENID_NS_AX)) {
+				MessageExtension ext = authReq
+						.getExtension(AxMessage.OPENID_NS_AX);
+				if (ext instanceof FetchRequest) {
+					FetchRequest fetchReq = (FetchRequest) ext;
+					Map required = fetchReq.getAttributes(true);
+					Map optional = fetchReq.getAttributes(false);
+					if (required.containsKey("email")
+							|| optional.containsKey("email")) {
+						Map userDataExt = new HashMap();
+						// userDataExt.put("email", userData.get(3));
+
+						FetchResponse fetchResp = FetchResponse
+								.createFetchResponse(fetchReq, userDataExt);
+						// (alternatively) manually add attribute values
+						fetchResp
+								.addAttribute(
+										"email",
+										"http://schema.openid.net/contact/email",
+										email);
+						response.addExtension(fetchResp);
+					}
+				} else // if (ext instanceof StoreRequest)
+				{
+					throw new UnsupportedOperationException("TODO");
+				}
+			}
+			if (authReq.hasExtension(SRegMessage.OPENID_NS_SREG)) {
+				MessageExtension ext = authReq
+						.getExtension(SRegMessage.OPENID_NS_SREG);
+				if (ext instanceof SRegRequest) {
+					SRegRequest sregReq = (SRegRequest) ext;
+					List required = sregReq.getAttributes(true);
+					List optional = sregReq.getAttributes(false);
+					if (required.contains("email")
+							|| optional.contains("email")) {
+						// data released by the user
+						Map userDataSReg = new HashMap();
+						userDataSReg.put("email", "user@example.com");
+
+						SRegResponse sregResp = SRegResponse
+								.createSRegResponse(sregReq, userDataSReg);
+						// (alternatively) manually add attribute values
+						// sregResp.addAttribute("email", email);
+						response.addExtension(sregResp);
+						try {
+							manager.sign((AuthSuccess) response);
+						} catch (ServerException e) {
+							log.error("", e);
+						} catch (AssociationException e) {
+							log.error("", e);
+						}
+					}
+				} else {
+					throw new UnsupportedOperationException("TODO");
+				}
+			}
+			// caller will need to decide which of the following to use:
+
+			// option1: GET HTTP-redirect to the return_to URL
+			String destUrl = response.getDestinationUrl(true);
+			log.debug(destUrl);
+			httpResp.sendRedirect(destUrl);
+
+			// option2: HTML FORM Redirection
+			// RequestDispatcher dispatcher =
+			// getServletContext().getRequestDispatcher("formredirection.jsp");
+			// httpReq.setAttribute("prameterMap",
+			// response.getParameterMap());
+			// httpReq.setAttribute("destinationUrl",
+			// response.getDestinationUrl(false));
+			// dispatcher.forward(request, response);
+			// return null;
+		}
+	}
+
+	private String directResponse(HttpServletResponse httpResp, String response)
+			throws IOException {
+		ServerServlet.writeResponse(httpResp, response);
+		return null;
+	}
 }
