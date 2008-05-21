ea1838865274f73b474063b5b7c18bbfe771f89b
zhoushuqun <zhoushuqun>
2008-05-21 13:59:23 +0000

    Add mindmap of Java OpenID Server.

---
+++ doc/jos.mm
@@ -0,0 +1,50 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1211376488068" ID="Freemind_Link_466481744" MODIFIED="1211378003282" TEXT="jos">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Java OpenID Server</text>
+</hook>
+<node CREATED="1211376556583" ID="_" MODIFIED="1211376768813" POSITION="right" TEXT="Sign up"/>
+<node CREATED="1211376693470" ID="Freemind_Link_1052103579" MODIFIED="1211376772853" POSITION="right" TEXT="Sign in"/>
+<node CREATED="1211376731820" ID="Freemind_Link_1918567677" MODIFIED="1211377054571" POSITION="right" TEXT="Account settings">
+<node CREATED="1211376810168" FOLDED="true" ID="Freemind_Link_1652980491" MODIFIED="1211377038199" TEXT="Manage e-mail addresses">
+<node CREATED="1211376946732" ID="Freemind_Link_1839523349" MODIFIED="1211377327082" TEXT="Add a new e-mail address">
+<node CREATED="1211377070183" ID="Freemind_Link_1646889019" MODIFIED="1211377077050" TEXT="Send instructions"/>
+<node CREATED="1211377085079" ID="Freemind_Link_870309803" MODIFIED="1211377089650" TEXT="Active"/>
+</node>
+<node CREATED="1211376893918" ID="Freemind_Link_639483267" MODIFIED="1211376896657" TEXT="Make primary"/>
+<node CREATED="1211376932164" ID="Freemind_Link_46576681" MODIFIED="1211376934752" TEXT="Remove"/>
+</node>
+<node CREATED="1211377159788" FOLDED="true" ID="Freemind_Link_1800735718" MODIFIED="1211377182126" TEXT="Authentication settings">
+<node CREATED="1211376841175" ID="Freemind_Link_1808724230" MODIFIED="1211376858922" TEXT="Change password"/>
+<node CREATED="1211377353861" ID="Freemind_Link_1213655722" MODIFIED="1211377363011" TEXT="Remove account password"/>
+<node CREATED="1211377183355" ID="Freemind_Link_51448672" MODIFIED="1211377222158" TEXT="Manage SSL client certificates">
+<node CREATED="1211377233924" ID="Freemind_Link_563467119" MODIFIED="1211377331642" TEXT="Add a client certificate"/>
+<node CREATED="1211377273464" ID="Freemind_Link_1830540128" MODIFIED="1211377336873" TEXT="Revoke a certificate"/>
+</node>
+</node>
+<node CREATED="1211377389876" FOLDED="true" ID="Freemind_Link_1671965080" MODIFIED="1211377393416" TEXT="Personas">
+<node CREATED="1211377415515" ID="Freemind_Link_1521736720" MODIFIED="1211377419998" TEXT="Add a persona"/>
+<node CREATED="1211377420451" ID="Freemind_Link_1656361366" MODIFIED="1211377423910" TEXT="Edit a persona"/>
+<node CREATED="1211377428115" ID="Freemind_Link_1249527444" MODIFIED="1211377432374" TEXT="Delete a persona"/>
+<node CREATED="1211377443267" ID="Freemind_Link_106541614" MODIFIED="1211377449533" TEXT="Set an image for a persona"/>
+</node>
+<node CREATED="1211377466594" ID="Freemind_Link_846240310" MODIFIED="1211378028957" TEXT="Identity page"/>
+<node CREATED="1211377510385" ID="Freemind_Link_931217580" MODIFIED="1211377513691" TEXT="Delete account"/>
+</node>
+<node CREATED="1211377548501" ID="Freemind_Link_535657747" MODIFIED="1211377556218" POSITION="right" TEXT="Account History">
+<node CREATED="1211377557766" ID="Freemind_Link_1524650077" MODIFIED="1211377561977" TEXT="Visited sites"/>
+<node CREATED="1211377563134" ID="Freemind_Link_487091470" MODIFIED="1211377569689" TEXT="Recent activity">
+<node CREATED="1211377638756" ID="Freemind_Link_1900068706" MODIFIED="1211377646319" TEXT="Recent events"/>
+<node CREATED="1211377649275" ID="Freemind_Link_695408157" MODIFIED="1211377651630" TEXT="Sessions">
+<node CREATED="1211377660955" ID="Freemind_Link_1779049700" MODIFIED="1211377664758" TEXT="Activity"/>
+</node>
+</node>
+</node>
+<node CREATED="1211377800230" ID="Freemind_Link_1719824803" MODIFIED="1211377803161" POSITION="right" TEXT="Your domains">
+<node CREATED="1211377811421" ID="Freemind_Link_327554269" MODIFIED="1211377821984" TEXT="Set up a new domain"/>
+</node>
+<node CREATED="1211377896075" ID="Freemind_Link_1511801096" MODIFIED="1211377904798" POSITION="right" TEXT="Your affiliate sites"/>
+<node CREATED="1211377919994" ID="Freemind_Link_1229179598" MODIFIED="1211377922013" POSITION="right" TEXT="Sign out"/>
+</node>
+</map>
