17da55f5ad897620c6a38cb8fdb3bc68e4bdef23
zhoushuqun <zhoushuqun>
2009-04-14 19:10:41 +0000

    Use springMacroRequestContext.locale instead of locale that defined by myself.

--- jos-web/src/main/java/cn/net/openid/jos/web/controller/HumanLanguageSelectionController.java
+++ jos-web/src/main/java/cn/net/openid/jos/web/controller/HumanLanguageSelectionController.java
@@ -6,7 +6,6 @@ package cn.net.openid.jos.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.net.openid.jos.web.AbstractJosController;
@@ -15,39 +14,21 @@ import cn.net.openid.jos.web.AbstractJosController;
  * Human language selection controller.
  * 
  * @author Sutra Zhou
- * 
  */
 public class HumanLanguageSelectionController extends AbstractJosController {
 	private ModelAndView modelAndView;
 
-	private LocaleResolver localeResolver;
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.web.servlet.mvc.Controller#handleRequest(javax.servlet
-	 * .http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	/**
+	 * {@inheritDoc}
 	 */
 	public ModelAndView handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		request.setAttribute("locales", this.getJosService()
 				.getAvailableLocales());
-		request.setAttribute("locale", this.localeResolver
-				.resolveLocale(request));
 		return modelAndView;
 	}
 
 	public void setFormView(String formView) {
 		this.modelAndView = new ModelAndView(formView);
 	}
-
-	/**
-	 * @param localeResolver
-	 *            the localeResolver to set
-	 */
-	public void setLocaleResolver(LocaleResolver localeResolver) {
-		this.localeResolver = localeResolver;
-	}
-
 }
